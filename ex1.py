f834d32796ec77f409289bea4fd7d28178a3f0a3
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-11-29 10:38:02 +0000

    ex2 e 3

--- ex1.py
+++ ex1.py
@@ -2,3 +2,5 @@ def main():
     print("What is your name?")
     yourName=input()
     print("Hello {0}".format(yourName))
+    yourAge=int(input("What's your age?"))
+    print("Hello {0} you are {1} years old".format(yourName,yourAge))
