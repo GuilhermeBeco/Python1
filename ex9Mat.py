898e2e91966f45929a2d0aa9a207e727aad97e61
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-11-29 15:34:13 +0000

    correct

--- ex9Mat.py
+++ ex9Mat.py
@@ -3,4 +3,4 @@ def main():
     valDolar=float(input("Insert the amount to convert: "))
     rate=0.88
     valEuro=int(valDolar*rate)
-    print("{0} dolars equals to {1}€".format(valDolar,valEuro))
+    print("{0} dolars equals to {1}€ (with cents={2} )".format(valDolar,valEuro,(valDolar*rate)))
