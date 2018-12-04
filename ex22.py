64546c8d03fae6c2ec95e2c86cf65b9ef6d415c0
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-12-04 10:18:29 +0000

    ex 21 e 22

---
+++ ex22.py
@@ -0,0 +1,8 @@
+def main():
+    switch1=int(input("Qual o estado do primeiro switch: "))
+    switch2=int(input("Qual o estado do segundo switch: "))
+
+    if switch1==1 or switch2==1:
+        print("A luz está ligada")
+    else:
+        print("A luz está desligada")
