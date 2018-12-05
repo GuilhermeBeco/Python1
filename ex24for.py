f92caba0b26c7adb2b9b0d62bb917462fccb6ae5
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-12-05 10:58:00 +0000

    jf

---
+++ ex24for.py
@@ -0,0 +1,7 @@
+def main():
+    str=input("Qual a mensagem que quer apresesntar: ")
+    vezes=int(input("Quantas vezes a quer apresentar: "))
+
+    for counter in range(0,vezes):
+        print(str)
+        print()
