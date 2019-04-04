6d8ec7ec7b4a8d00697a54b6db0d61e3612da3f2
GuilhermeBeco <2171293@my.ipleiria.pt>
2019-04-04 11:26:27 +0100

    db

--- ex61.py
+++ ex61.py
@@ -1,11 +1,25 @@
 import sqlite3
 
 
-def create_table(db_name,sql):
+def create_table(db_name,sql,table_name):
     with sqlite3.connect(db_name) as db:
         cursor=db.cursor()
-        cursor.execute(sql)
-        db.commit()
+        cursor.execute("Select name from sqlite_master where name=?",(table_name,))
+        result=cursor.fetchall()
+        keep_table=True
+        if len(result)==1:
+            response=int(input("the table {0} already exists, do you wish to recreate it (1/0)".format((table_name))))
+            if response==1:
+                keep_table=False
+                print("The table will be created, all data will be deleted")
+                cursor.execute("Drop table if exists {0}".format(table_name))
+            else:
+                print("The existing table was kept")
+        else:
+            keep_table=False
+        if not keep_table:
+            cursor.execute(sql)
+            db.commit()
 if __name__ == '__main__':
     db_name = "Coffe_shop.db"
     sql = """create table Product
@@ -13,4 +27,4 @@ if __name__ == '__main__':
     Name varchar ,
     Price real,
     primary key(ProductID))"""
-    create_table(db_name,sql)
+    create_table(db_name,sql,"Product")
